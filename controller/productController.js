df347a04f7a651d0bf0f150e129ecf275377dc16
Deepanshu-Mehta <146458164+Deepanshu-Mehta@users.noreply.github.com>
2024-12-02 11:07:04 +0530

    updated

--- controller/productController.js
+++ controller/productController.js
@@ -24,7 +24,7 @@ const cancelShipping = async (req,res)=>{
         if(!product){
             return res.status(400).json({})
         }
-        const cancelshipp = await prisma.Shipping.updata({
+        const cancelshipp = await prisma.Shipping.update({
             where : {id : shippingId},
             data : {status : "cancelled" }
         });
