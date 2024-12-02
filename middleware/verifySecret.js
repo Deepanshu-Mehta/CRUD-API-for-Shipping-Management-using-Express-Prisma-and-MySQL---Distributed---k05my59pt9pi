6a81bc69b659d2c2a24bf2d496739d7cbe3c3432
Deepanshu-Mehta <146458164+Deepanshu-Mehta@users.noreply.github.com>
2024-12-02 11:04:35 +0530

    updated

--- middleware/verifySecret.js
+++ middleware/verifySecret.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
 
 const verifySecret = (req,res,next)=>{
-    const {SHIPPING_SECRET_KEY} = req.headers;
-    if(!SHIPPING_SECRET_KEY){
+    const key = req.headers.shipping_secret_key;
+    
+    if(!key){
         return res.status(403).json({error: "SHIPPING_SECRET_KEY is missing or invalid"})
     }
-    if(SHIPPING_SECRET_KEY !== process.env.SHIPPING_SECRET_KEY){
+    if(key !== process.env.SHIPPING_SECRET_KEY){
         return res.status(403).json({error: "Failed to authenticate SHIPPING_SECRET_KEY"})
     }
     next();
